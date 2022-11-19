42f6332ffe04bb6708aa87fe1828809865873fcd
amusinbego1 <amusinbego1@etf.unsa.ba>
2022-11-19 19:57:13 +0100

    Made "App" class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,18 +1,24 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
-
 /**
  * @author Amer Musinbegovic
- *
+ * Class that uses first element of parameter args in main method to evaluate the expression
  */
 public class App
 {
+    /**
+     * Uses ExpressionEvaluator class to evaluate the given expression
+     * @param args first element should be expression in from of String
+     * @throws RuntimeException if expression is invalid (valid expression has a space between every charachter)
+     */
     public static void main( String[] args )
     {
-        String s = "amer plaho     jede govana";
-        String[] s2 = s.split("\\s+");
-        for(String t: s2)
-            System.out.println(t);
+        try{
+            ExpressionEvaluator evaluator = new ExpressionEvaluator();
+            System.out.println(evaluator.evaluate(args[0]));
+        }
+        catch(Exception e){
+            throw new RuntimeException("Invalid expression");
+        }
     }
 }
