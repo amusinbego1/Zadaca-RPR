d95865465994ad781f286d12b8e6405e210057f1
amusinbego1 <amusinbego1@etf.unsa.ba>
2022-11-19 22:30:36 +0100

    Changed spelling error in description

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -8,7 +8,7 @@ public class App
 {
     /**
      * Uses ExpressionEvaluator class to evaluate the given expression
-     * @param args first element should be expression in from of String
+     * @param args first element should be expression in form of a String
      * @throws RuntimeException if expression is invalid (valid expression has a space between every character)
      */
     public static void main( String[] args )
