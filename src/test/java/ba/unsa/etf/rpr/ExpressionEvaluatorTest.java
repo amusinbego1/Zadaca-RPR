ea1213e3b59e2fab96738dc9898b62cb3ae422cb
amusinbego1 <amusinbego1@etf.unsa.ba>
2022-11-19 22:17:28 +0100

    Added more tests and enabled lambda expressions

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -6,8 +6,31 @@ import static org.junit.Assert.*;
 public class ExpressionEvaluatorTest {
 
     @Test
-    public void evaluate(){
+    public void evaluate1(){
         ExpressionEvaluator evaluator = new ExpressionEvaluator();
-        assertEquals(9, (int)evaluator.evaluate("( 6 + 3 )"));
+        assertEquals(9.0, evaluator.evaluate("( 6 + 3 )"), 0.001);
+    }
+
+    @Test
+    public void evaluate2(){
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        assertEquals(3.0, evaluator.evaluate("( sqrt ( ( ( ( 6 + 3 ) * 3 ) - 0 ) / 3 ) )"), 0.001);
+    }
+
+    @Test
+    public void evaluate4(){
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        assertThrows(Exception.class, () ->{evaluator.evaluate("( sqrt ( ( ( ( 6 + 3 ) * 3 ) - 1 ) / 0 ))");});
+    }
+    @Test
+    public void evaluate3(){
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        assertThrows(Exception.class, () ->{evaluator.evaluate("(sqrt ( ( ( ( 6 +3) * 3 ) - 0 ) / 3 ) )");});
+    }
+
+    @Test
+    public void evaluate5(){
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        assertEquals(-3.0, evaluator.evaluate("( 6 - 9 )"), 0.001);
     }
 }
